5c47b4387e6709c5719ffee18c467db6c92ebb0c
Lazycoder97 <lazycoder97@gmail.com>
2016-05-11 19:29:18 +0700

    First upload

---
+++ MultiThreadRun/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace MultiThreadRun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmdPathTextBox = new System.Windows.Forms.TextBox();
+            this.runButton = new System.Windows.Forms.Button();
+            this.numThreadBox = new System.Windows.Forms.NumericUpDown();
+            this.cmdFileBrowseButton = new System.Windows.Forms.Button();
+            this.fileOpenner = new System.Windows.Forms.OpenFileDialog();
+            this.statusTextBox = new System.Windows.Forms.RichTextBox();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.memoryLimiterPathBox = new System.Windows.Forms.TextBox();
+            this.useMemoryLimiterCheckBox = new System.Windows.Forms.CheckBox();
+            this.memoryLimiterBrowseButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numThreadBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmdPathTextBox
+            // 
+            this.cmdPathTextBox.Location = new System.Drawing.Point(12, 12);
+            this.cmdPathTextBox.Name = "cmdPathTextBox";
+            this.cmdPathTextBox.Size = new System.Drawing.Size(400, 22);
+            this.cmdPathTextBox.TabIndex = 0;
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(418, 39);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 23);
+            this.runButton.TabIndex = 1;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.RunButton_Click);
+            // 
+            // numThreadBox
+            // 
+            this.numThreadBox.Location = new System.Drawing.Point(350, 40);
+            this.numThreadBox.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numThreadBox.Name = "numThreadBox";
+            this.numThreadBox.Size = new System.Drawing.Size(62, 22);
+            this.numThreadBox.TabIndex = 3;
+            this.numThreadBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // cmdFileBrowseButton
+            // 
+            this.cmdFileBrowseButton.Location = new System.Drawing.Point(418, 10);
+            this.cmdFileBrowseButton.Name = "cmdFileBrowseButton";
+            this.cmdFileBrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.cmdFileBrowseButton.TabIndex = 4;
+            this.cmdFileBrowseButton.Text = "Browse";
+            this.cmdFileBrowseButton.UseVisualStyleBackColor = true;
+            this.cmdFileBrowseButton.Click += new System.EventHandler(this.cmdFileBrowseButton_Click);
+            // 
+            // statusTextBox
+            // 
+            this.statusTextBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.statusTextBox.Location = new System.Drawing.Point(12, 125);
+            this.statusTextBox.Name = "statusTextBox";
+            this.statusTextBox.ReadOnly = true;
+            this.statusTextBox.Size = new System.Drawing.Size(481, 310);
+            this.statusTextBox.TabIndex = 5;
+            this.statusTextBox.Text = "";
+            this.statusTextBox.WordWrap = false;
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(418, 39);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 6;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            // 
+            // memoryLimiterPathBox
+            // 
+            this.memoryLimiterPathBox.Enabled = false;
+            this.memoryLimiterPathBox.Location = new System.Drawing.Point(12, 97);
+            this.memoryLimiterPathBox.Name = "memoryLimiterPathBox";
+            this.memoryLimiterPathBox.Size = new System.Drawing.Size(400, 22);
+            this.memoryLimiterPathBox.TabIndex = 7;
+            // 
+            // useMemoryLimiterCheckBox
+            // 
+            this.useMemoryLimiterCheckBox.AutoSize = true;
+            this.useMemoryLimiterCheckBox.Location = new System.Drawing.Point(12, 70);
+            this.useMemoryLimiterCheckBox.Name = "useMemoryLimiterCheckBox";
+            this.useMemoryLimiterCheckBox.Size = new System.Drawing.Size(163, 21);
+            this.useMemoryLimiterCheckBox.TabIndex = 8;
+            this.useMemoryLimiterCheckBox.Text = "Use Memory Limiter :";
+            this.useMemoryLimiterCheckBox.UseVisualStyleBackColor = true;
+            this.useMemoryLimiterCheckBox.CheckedChanged += new System.EventHandler(this.useMemoryLimiterCheckBox_CheckedChanged);
+            // 
+            // memoryLimiterBrowseButton
+            // 
+            this.memoryLimiterBrowseButton.Enabled = false;
+            this.memoryLimiterBrowseButton.Location = new System.Drawing.Point(418, 96);
+            this.memoryLimiterBrowseButton.Name = "memoryLimiterBrowseButton";
+            this.memoryLimiterBrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.memoryLimiterBrowseButton.TabIndex = 9;
+            this.memoryLimiterBrowseButton.Text = "Browse";
+            this.memoryLimiterBrowseButton.UseVisualStyleBackColor = true;
+            this.memoryLimiterBrowseButton.Click += new System.EventHandler(this.memoryLimiterBrowseButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 447);
+            this.Controls.Add(this.memoryLimiterBrowseButton);
+            this.Controls.Add(this.useMemoryLimiterCheckBox);
+            this.Controls.Add(this.memoryLimiterPathBox);
+            this.Controls.Add(this.statusTextBox);
+            this.Controls.Add(this.cmdFileBrowseButton);
+            this.Controls.Add(this.numThreadBox);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.cmdPathTextBox);
+            this.Controls.Add(this.stopButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "MultiThreadRun";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.numThreadBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox cmdPathTextBox;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.NumericUpDown numThreadBox;
+        private System.Windows.Forms.Button cmdFileBrowseButton;
+        private System.Windows.Forms.OpenFileDialog fileOpenner;
+        private System.Windows.Forms.RichTextBox statusTextBox;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TextBox memoryLimiterPathBox;
+        private System.Windows.Forms.CheckBox useMemoryLimiterCheckBox;
+        private System.Windows.Forms.Button memoryLimiterBrowseButton;
+    }
+}
+
---
+++ MultiThreadRun/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Concurrent;
+
+namespace MultiThreadRun
+{
+    public partial class Form1 : Form
+    {
+        const ulong ensuranceMem = 314572800;   // The amount of available memory to leave out
+
+        ConcurrentQueue<string> cmdQueue;
+        Process[] processes;
+        string workingDir;
+        int endCheckCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            numThreadBox.Maximum = Environment.ProcessorCount - 1;
+
+            timer.Tick += delegate
+            {
+                lock (processes)
+                    for (int i = 0; i < processes.Length; ++i)
+                        if (!processes[i].HasExited) return;
+
+                ++endCheckCount;
+                if (endCheckCount == 3)
+                {
+                    if (!cmdQueue.IsEmpty) statusTextBox.AppendText("Error : can't start anymore processes!");
+                    toggleControls();
+                    timer.Enabled = false;
+                }
+            };
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            lock (runButton)
+                if (!runButton.Visible) stopButton_Click(null, null);
+        }
+
+        private void RunButton_Click(object sender, EventArgs e)
+        {
+            toggleControls();
+            statusTextBox.Text = "";
+
+            loadCmdQueue(cmdPathTextBox.Text);
+            processes  = new Process[(int)numThreadBox.Value];
+            workingDir = Path.GetDirectoryName(cmdPathTextBox.Text);
+            
+            for (int i = 0; i < processes.Length; ++i)
+            {
+                processes[i] = new Process();
+                processes[i].EnableRaisingEvents = true;
+                processes[i].Exited += doMoreCmd;
+            }
+            doMoreCmd(null, null);
+
+            timer.Enabled = true;
+        }
+
+        private void cmdFileBrowseButton_Click(object sender, EventArgs e)
+        {
+            if (fileOpenner.ShowDialog() == DialogResult.OK)
+                cmdPathTextBox.Text = fileOpenner.FileName;
+        }
+
+        private void memoryLimiterBrowseButton_Click(object sender, EventArgs e)
+        {
+            if (fileOpenner.ShowDialog() == DialogResult.OK)
+                memoryLimiterPathBox.Text = fileOpenner.FileName;
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+
+            lock (processes)
+            {
+                for (int i = 0; i < processes.Length; ++i)
+                    if (!processes[i].HasExited)
+                        try
+                        {
+                            processes[i].Exited -= doMoreCmd;
+                            processes[i].Kill();
+                            processes[i].Refresh();
+                        } catch (Exception) { }
+            }
+
+            toggleControls();
+            cmdQueue = null;
+        }
+
+        private void useMemoryLimiterCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            memoryLimiterBrowseButton.Enabled = memoryLimiterPathBox.Enabled ^= true;
+        }
+
+        private ProcessStartInfo getProcessInfo(string cmd)
+        {
+            string name = cmd,
+                   args = "";
+            int splitIndex = cmd.IndexOf(' ');
+
+            if (splitIndex != -1)
+            {
+                name = cmd.Substring(0, splitIndex);
+                args = cmd.Substring(splitIndex + 1);
+            }
+
+            var info = new ProcessStartInfo();
+            info.FileName = name;
+            info.Arguments = args;
+            info.WindowStyle = ProcessWindowStyle.Minimized;
+            info.WorkingDirectory = workingDir;
+
+            return info;
+        }
+
+        private void loadCmdQueue(string path)
+        {
+            cmdQueue = new ConcurrentQueue<string>();
+
+            var fi = new StreamReader(path);
+            while (!fi.EndOfStream)
+            {
+                string cmd = fi.ReadLine();
+                if (!string.IsNullOrEmpty(cmd)) cmdQueue.Enqueue(cmd);
+            }
+
+            fi.Close();
+        }
+
+        private bool enoughMemoryForCmd (string cmd)
+        {
+            Process getMemoryProcess = new Process();
+            getMemoryProcess.StartInfo             = getProcessInfo(cmd);
+            getMemoryProcess.StartInfo.FileName    = memoryLimiterPathBox.Text;
+            getMemoryProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            getMemoryProcess.Start();
+            getMemoryProcess.WaitForExit();
+
+            if (getMemoryProcess.ExitCode != 0)
+            {
+                statusTextBox.AppendText("Memory limiter exited with error for : " + cmd + "\n");
+                return false;
+            }
+
+            ulong requiredMem  = ulong.Parse(getMemoryProcess.StandardOutput.ReadLine());
+            ulong availableMem = (new Microsoft.VisualBasic.Devices.ComputerInfo()).AvailablePhysicalMemory;
+
+            return requiredMem + ensuranceMem < availableMem;
+        }
+
+        private void doMoreCmd (object o,EventArgs e)
+        {
+            (o as Process)?.Refresh();
+
+            lock (processes)
+                while (true)
+                {
+                    string cmd;
+                    if (!cmdQueue.TryPeek(out cmd)) break;
+                    if (useMemoryLimiterCheckBox.Checked && !enoughMemoryForCmd(cmd)) break;
+
+                    bool stopFlag = true;
+                    for (int i = 0; i < processes.Length; ++i)
+                    {
+                        try
+                        {
+                            if (!processes[i].HasExited) continue;
+                        }
+                        catch (InvalidOperationException) { }
+
+                        lock (statusTextBox) statusTextBox.AppendText(cmd + "\n");
+                        processes[i].StartInfo = getProcessInfo(cmd);
+                        processes[i].Start();
+
+                        stopFlag = false;
+                        cmdQueue.TryDequeue(out cmd);
+                        break;
+                    }
+
+                    if (stopFlag) break;
+                }
+        }
+
+        private void toggleControls ()
+        {
+            if (runButton.Visible)
+            {
+                cmdPathTextBox.Enabled =
+                numThreadBox.Enabled =
+                useMemoryLimiterCheckBox.Enabled =
+                memoryLimiterPathBox.Enabled =
+                memoryLimiterBrowseButton.Enabled =
+                runButton.Visible =
+                false;
+            } else
+            {
+                cmdPathTextBox.Enabled =
+                numThreadBox.Enabled =
+                useMemoryLimiterCheckBox.Enabled =
+                runButton.Visible =
+                true;
+
+                memoryLimiterPathBox.Enabled = memoryLimiterBrowseButton.Enabled = useMemoryLimiterCheckBox.Checked;
+            }
+        }
+    }
+}
